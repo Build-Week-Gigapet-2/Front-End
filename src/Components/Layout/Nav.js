8c5022c57a82bb82fd967d0778b8e94fd6d99c41
skraus5628 <s_read@aol.com>
2020-01-29 21:03:11 -0600

    Set up initial routes/reducers
    added dependencies: Axios, react-router-dom
    added reducer folder, actions folder,
    and began setting up basic routing in the nav

---
+++ src/Components/Layout/Nav.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { Link } from "react-router-dom";
+
+
+const Nav = props => {
+
+    const logged = localStorage.getItem('token');
+
+
+    return (
+        <Fragment>
+            <div>
+                <Link to='/'>Home</Link>
+                {logged && <Link to='/'>Home</Link>}
+                {!logged && <Link to= '/Login'>Log In</Link>}
+                {logged && <button className='logout-btn' onClick>Logout</button>}
+                {!logged && <Link to='/Register'>Sign Up</Link>}
+                <Link to='/Search'>Search</Link>
+            </div>
+        </Fragment>
+    )
+}
+
+
+export default Nav;
---
+++ src/Reducers/auth.js
@@ -0,0 +1,66 @@
+import { userConstants } from '../actions/types';
+
+
+export const initialState = {
+    user: {
+        username: '',
+        password: '',
+        email: ''
+    },
+    isLoading: false,
+    isLoggedIn: false,
+    error: null
+}
+
+export default function authReducer(state = initialState, action){
+    switch(action.type) {
+        case userConstants.LOGIN_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+                error: null               
+            };
+        case userConstants.LOGIN_SUCCESS:
+            console.log(action)
+            return {
+                ...state,
+                user: action.payload,
+                isLoading: false,
+                isLoggedIn: true,
+                error: null
+            };
+        case userConstants.LOGIN_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: false,
+                error: 'Failed to log in'
+            };
+        case userConstants.LOGOUT:
+            console.log('logout')
+            return {
+                ...state,
+                isLoading: false,
+                isLoggedIn: false,
+                error: ''
+            };
+
+        case userConstants.REGISTER_REQUEST: 
+            return { 
+            ...state,
+            isLoading: true
+            };
+        case userConstants.REGISTER_SUCCESS:
+            return { 
+            ...state,
+            isLoading: false
+            };
+        case userConstants.REGISTER_FAILURE:
+            return { 
+            ...state,
+            error: 'Failed to Register'
+            };
+        default:
+            return state;
+    }
+}
