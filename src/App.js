a5482a6b46322727c30bc3150bd85f2b1f6f9e69
Barbara Mayfield <51219381+barbara-mayfield@users.noreply.github.com>
2020-01-31 18:55:22 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -55,20 +55,3 @@ export default function App() {
      
     </div>
   );
-import Nav from './Components/Layout/Nav';
-
-import './App.css';
-
-function App() {
-    return (
-        <div className="App">
-            <Nav />
-
-            <div className='content'>
-                <h2> Welcome to Gigapet</h2>
-                <p>If you see this, everything is running!</p>
-            </div>
-        </div>
-    );
-}
-
