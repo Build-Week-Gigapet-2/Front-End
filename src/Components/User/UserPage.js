709704fed5e5069f921a1fb790c4f3dcda3e24c3
skraus5628 <s_read@aol.com>
2020-02-03 01:15:48 -0600

    linked food form to user page

--- src/Components/User/UserPage.js
+++ src/Components/User/UserPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import FoodForm from './foodForm';
 
 export default function UserPage() {
     return (
         <div>
             <p>User's Page</p>
+            <FoodForm />
         </div>
     )
 }
--- src/Components/User/foodForm.js
+++ src/Components/User/foodForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function foodForm() {
     return (
         <div>
-            
+            <p>food form here</p>
         </div>
     )
 }
