e0c9b86ef1752397dbe343bbdf8932899d378551
Christian <Chrisaduenas@gmail.com>
2020-02-03 12:31:06 -0800

    Touchups to the link styling

--- src/Components/User/Login.js
+++ src/Components/User/Login.js
@@ -59,19 +59,6 @@ function Login(props) {
         }
       });
 
-    const submitButton = makeStyles({
-        root: {
-          background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-          border: 0,
-          borderRadius: 3,
-          boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-          color: "white",
-          height: 25,
-          padding: "0 20px",
-          margin: '20px 0'
-        }
-      });
-
     const styledLink = makeStyles({
         root:{
             textDecoration: 'none',
@@ -80,8 +67,6 @@ function Login(props) {
         }
     })
 
-
-    const button = submitButton();
     const images = imageStyle();
     const forms = styledForm();
     const inputs = styledInput();
--- src/Components/User/Register.js
+++ src/Components/User/Register.js
@@ -55,19 +55,6 @@ function Register(props) {
         }
       });
 
-    const submitButton = makeStyles({
-        root: {
-          background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-          border: 0,
-          borderRadius: 3,
-          boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-          color: "white",
-          height: 25,
-          padding: "0 20px",
-          margin: '20px 0'
-        }
-      });
-
     const styledLink = makeStyles({
         root:{
             textDecoration: 'none',
@@ -77,7 +64,6 @@ function Register(props) {
     })
 
 
-    const button = submitButton();
     const images = imageStyle();
     const forms = styledForm();
     const inputs = styledInput();
