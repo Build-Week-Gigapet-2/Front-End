a4c83e333b0f01140cf7efcf3bc53e4200afa893
Harry Merritt <harry@merritt.es>
2020-02-05 01:12:23 +0000

    Show loader and hide graph when fetching data from api

--- src/Components/Children/foodGraph.js
+++ src/Components/Children/foodGraph.js
@@ -3,8 +3,8 @@ import AxiosWithAuth from '../../Utils/AxiosWithAuth';
 import { CircularProgress, Typography } from '@material-ui/core';
 import { ResponsivePie } from '@nivo/pie';
 
-export default function FoodGraph(props) {
-
+export default function FoodGraph(props)
+{
     //  Store graph data
     //  -> childs diet intake
     const [graphData, setGraphData] = useState([
@@ -17,6 +17,9 @@ export default function FoodGraph(props) {
         {id:"Treats", label:"Treats", value:1}
     ]);
 
+    //  Is graph data being loaded
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         AxiosWithAuth()
           .get('/api/users/:id/children/:id/food')
@@ -25,7 +28,7 @@ export default function FoodGraph(props) {
           })
           .catch((err) => {
               console.log('[FoodGraph] Unable to get graph data', err);
-          })
+          });
     }, []);
 
     return (
@@ -33,36 +36,42 @@ export default function FoodGraph(props) {
             <Typography variant="h3" component="h1">
                 Breakdown of Food <strong>Intake</strong>
             </Typography>
-            <div style={{width: '100%', height: '500px'}}>
-                <div>
-                    <CircularProgress />
-                </div>
-                <ResponsivePie
-                    data={graphData}
-                    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-                    innerRadius={0.5}
-                    padAngle={0.7}
-                    legends={[
-                        {
-                            anchor: 'bottom',
-                            direction: 'row',
-                            translateY: 56,
-                            itemWidth: 100,
-                            itemHeight: 18,
-                            itemTextColor: '#999',
-                            symbolSize: 18,
-                            symbolShape: 'circle',
-                            effects: [
-                                {
-                                    on: 'hover',
-                                    style: {
-                                        itemTextColor: '#000'
+            <div style={{position: 'relative'}}>
+                {
+                    /* Show loading icon when fetching graph data */
+                    isLoading &&
+                    <div className='center center-full'>
+                        <CircularProgress />
+                    </div>
+                }
+                <div className={isLoading ? 'hide' : 'graph'} style={{position: 'relative', width: '100%', height: '500px'}}>
+                    <ResponsivePie
+                        data={graphData}
+                        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                        innerRadius={0.5}
+                        padAngle={0.7}
+                        legends={[
+                            {
+                                anchor: 'bottom',
+                                direction: 'row',
+                                translateY: 56,
+                                itemWidth: 100,
+                                itemHeight: 18,
+                                itemTextColor: '#999',
+                                symbolSize: 18,
+                                symbolShape: 'circle',
+                                effects: [
+                                    {
+                                        on: 'hover',
+                                        style: {
+                                            itemTextColor: '#000'
+                                        }
                                     }
-                                }
-                            ]
-                        }
-                    ]}
-                />
+                                ]
+                            }
+                        ]}
+                    />
+                </div>
             </div>
         </div>
     )
