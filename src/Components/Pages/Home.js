e8436421bff99ab671cc95d74a62df329419a765
Harry Merritt <harry@merritt.es>
2020-02-07 01:40:47 +0000

    Added subtitle to home page

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -6,9 +6,17 @@ export default function Home() {
     return (
         <div>
             <Typography variant="h1" component="h1">
-                Welcome to <strong>Gigapet</strong> 
+                Welcome to <strong>Gigapet</strong>
             </Typography>
-         
+            <Typography variant="h4" component="h4">
+                Helping parents track their children's nutrition with an interactive pet
+            </Typography>
+
+            <Typography variant="p" component="p">
+                Getting our kids to eat healthy can be a daunting task or epic struggle full of chasing, throwing, and straight out refusal.
+                With Gigapet, your child can watch their pet grow with every healthy choice they make,
+                encouraging them to eat healthier and allowing them the chance to have fun doing it!
+            </Typography>
         </div>
     )
 }
